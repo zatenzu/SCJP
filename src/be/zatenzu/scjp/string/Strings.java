cdd41d2ca2411e5d86b29a0b549afb7634c6902c
zatenzu <zatenzu@hotmail.com>
2015-01-07 16:35:09 +0100

    + concatenation

--- src/be/zatenzu/scjp/string/Strings.java
+++ src/be/zatenzu/scjp/string/Strings.java
@@ -26,5 +26,15 @@ public class Strings
     //f and g refere to own objet.
     //It's impossible to have two reference variables who refere to the same String!!!
     //It's impossible to change a instance of String, a new instance is create each time.
+    
+    /*************************************/
+   /** Concatenation                   **/
+  /*************************************/
+    int r = 4;
+    int ff = 4;
+    System.out.println("String" + r + ff);
+    System.out.println("String" + ( r + ff ));
+    //If the left or the rigth operator is a String, + will be a String concatenation
+    //If both operands are numeric, + will be an addition operator
   }
 }
