2909133690422624f1729a0d210862f8e369b19a
zatenzu <zatenzu@hotmail.com>
2015-01-07 14:00:26 +0100

    + Overloading with widening, boxing and var-args

--- src/be/zatenzu/scjp/overloading/a/MyClass.java
+++ src/be/zatenzu/scjp/overloading/a/MyClass.java
@@ -33,5 +33,124 @@ public class MyClass
     myClass.a(c);
     // ====> The reference type(NOT the object type) decide which overloaded method is invoke !!!!
     //It's decided at compiler time, not runtime
+    
+      /*************************************/
+     /** Overloading and primitive types **/
+    /*************************************/
+    
+    /** Widening vs Boxing! **/
+    //primitive argument -> primitive method parameter
+    byte bb = 0;
+    short s = 0;
+    long l = 0L;
+    float f = 0.0f;
+    myMethod(bb);
+    myMethod(s);
+    myMethod(l);
+    myMethod(f);
+    /*
+    ==> If the JVM doesn't find the method with exactly the same primitive type
+    the JVM use the smallest correspondent primitive type.
+    The smallest primitive type but bigger than the original argument type.
+    --> It's called Widening!
+    */
+
+    //Wrapper argument -> primitive method parameter    
+    Byte bbb = 0;
+    Short ss = 0;
+    Long ll = 0L;
+    Float ff = 0.0f;
+    myMethod(bbb);
+    myMethod(ss);
+    myMethod(ll);
+    myMethod(ff);
+    /*
+    ==> Idem with Wrapper class!
+    ==> If the JVM doesn't find the method with exactly the same Wrapper type
+    the JVM use the smallest correspondent primitive type!
+     */
+    
+    //primitive argument -> wrapper method parameter
+    short sssss = (short)1;
+    myMethodShort(sssss);
+    /*
+       ==> The JVM choses the non primitive type!!!
+    */
+    
+    //wrapper argument -> wrapper method parameter
+//    myMethodWrapper(bbb);//Forbidden!
+//    myMethodWrapper(ss);//Forbidden!
+    myMethodWrapper(ll);
+//    myMethodWrapper(ff);//Forbidden!
+    /*
+       /!\ Widening doesn't work with Wrapper arguments -> wrapper method parameter!!!
+    */
+    /** Conclusion:Widening wins! **/
+    
+    
+    /*************************************/
+   /** Overloading with var-args       **/
+  /*************************************/
+    /** Widening vs var-args**/
+    //primtive arguments -> primitives parameters
+    byte b1 = (byte)0;
+    byte b2 = (byte)0;
+    myMethod(b1,b2);
+    //JVM choses widening
+    
+    //Wrapper arguments -> primitives parameters
+    Byte bb1 = (byte)0;
+    Byte bb2 = (byte)0;
+    myMethod(b1,b2);
+    //JVM choses widening
+    /** Widening wins! **/
+    
+    /** Boxing vs var-args**/
+    //primitive arguments -> wrapper parameters
+    myMethodWrapper(b1,b2);
+    //=>JVM choses Boxing
+    /** Boxing wins! **/
+    
+    /** Cannot do widening + boxing **/
+//    myMethodWrapper2(b1);//forbidden
+    
+    //widening + var-args ->  OK
+    myMethodWrapper3(b1,b2);
+    /** But boxing + var-args -> OK **/
+    myMethodWrapper3(b1,b2);
+    /** boxing + IS-A test -> OK **/
+    myMethodObject((byte)1);
+    //=> byte -boxing-> Byte -ISA-> Object
+
+    /*************************************/
+   /** See page 254 for rules          **/
+  /*************************************/    
+    
   }
+  
+  public static void myMethod(int i){System.out.println("INT");}
+  public static void myMethod(long l){System.out.println("LONG");}
+  public static void myMethod(double d){System.out.println("DOUBLE");}
+  
+  public static void myMethod(Integer i){System.out.println("INT Wrapper");}
+  public static void myMethod(Long l){System.out.println("LONG Wrapper");}
+  public static void myMethod(Double d){System.out.println("DOUBLE Wrapper");}
+  
+  public static void myMethodShort(Short s){System.out.println("Short short");}
+  public static void myMethodShort(int i){System.out.println("Int short");}
+  
+  public static void myMethodWrapper(Integer i){System.out.println("Wrapper INT Wrapper");}
+  public static void myMethodWrapper(Long l){System.out.println("Wrapper LONG Wrapper");}
+  public static void myMethodWrapper(Double d){System.out.println("Wrapper DOUBLE Wrapper");}  
+  
+  public static void myMethod(int x, int y){System.out.println("int int");}
+  public static void myMethod(byte... args){System.out.println("byte...");}
+  
+  public static void myMethodWrapper(Byte x, Byte y){System.out.println("Wrapper byte byte");}
+  public static void myMethodWrapper(Byte... args){System.out.println("Wrapper byte...");}
+  
+  public static void myMethodWrapper2(Integer i){System.out.println("Wrapper2 int");}
+  public static void myMethodWrapper3(int... args){System.out.println("Wrapper3 int...");}
+  
+  public static void myMethodObject(Object o){System.out.println("Object");}
 }
