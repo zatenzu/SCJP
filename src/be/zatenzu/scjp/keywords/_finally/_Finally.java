80942a4ba5b338de50eb4ff6f40c153bf20fa875
zatenzu <zatenzu@hotmail.com>
2015-01-08 13:40:31 +0100

    + add content

--- src/be/zatenzu/scjp/keywords/_finally/_Finally.java
+++ src/be/zatenzu/scjp/keywords/_finally/_Finally.java
@@ -3,7 +3,23 @@ package be.zatenzu.scjp.keywords._finally;
 
 public class _Finally
 {
-  /**
-   * nothing special, see Chapter 5 for exception management
-   */
+  public static void main(String[] args)
+  {
+    // finally can be used without catch block
+    try{
+      System.out.println("");
+    }finally{}
+    
+    // If catch block exists, finally must be after the last catch block.
+    try{
+      System.out.println("");
+    }
+    catch(Exception ex){
+      
+    }
+    catch(Throwable t){}
+    finally{}
+    
+    //See try and catch keywords
+  }
 }
