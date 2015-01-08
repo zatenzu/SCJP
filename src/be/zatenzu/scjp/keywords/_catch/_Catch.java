edd5d4e5baee51d6c3f07290ec8f433f5eed2830
zatenzu <zatenzu@hotmail.com>
2015-01-08 13:40:12 +0100

    + catch with hierachical exception

--- src/be/zatenzu/scjp/keywords/_catch/_Catch.java
+++ src/be/zatenzu/scjp/keywords/_catch/_Catch.java
@@ -1,9 +1,28 @@
 // $Id$
 package be.zatenzu.scjp.keywords._catch;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class _Catch
 {
   /**
    * nothing special, see Chapter 5 for exception management
    */
+  //See try and finally keywords
+  //all error, exception, runtimeexception and throwable can be catch
+  
+  public static void main(String[] args)
+  {
+    try{
+      if(1 < 0){//I know, it's stupid
+        throw new MotherException(); 
+      }else{
+        throw new SubClassException(); 
+      }
+    }
+    catch(MotherException ex){}
+//    catch(SubClassException fex){}//Forbidden, compiler error!
+    //SubClassException already catch by the MothException catch block
+  }
 }
