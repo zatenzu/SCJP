4000ff61dd74713f66599970e2d9a95d0517c2dc
zatenzu <zatenzu@hotmail.com>
2015-01-08 13:39:27 +0100

    + new VM arguments and assertion with a exp 2

--- src/be/zatenzu/scjp/keywords/_assert/_Assert.java
+++ src/be/zatenzu/scjp/keywords/_assert/_Assert.java
@@ -9,10 +9,30 @@ public class _Assert
    * If the boolean expression is false then throw new AssertionError
    */
   
-  // /!\ VM arg -> -ea (enable asserts)
+  // /!\ VM arg -> -ea or -enableassertions (enable asserts)
+  // /!\ VM arg -> -da or -disableassertions (disable asserts)
+  // By default it's disable
+  /** You can enable/disable assertion just for a class or package **/
+  //-ea be.zatenzu.scjp.keywords._assert._Assert --> enable just for this class
+  //-ea be.zatenzu.scjp.keywords._assert --> enable just for this package
+  //-ea be.zatenzu.scjp.keywords._assert... --> enable just for this package and its subpackages
   
   public static void main(String[] args)
   {
     assert (args.length > 0);
+    assert true;
+    assert false : new String("VALUE FOR ASSERTIONERROR CONSTRUCTOR!!!");
+    //assert exp1 : exp2;
+    //If exp1 is false, an AssertionError is build with exp2 as value, this error is thrown
+    //Exp2 must return a value !!!
+    
+    
+    // /!\ LEGAL != APPROPRIATE && LEGAL != CORRECT
+    // Appropriate and correct words mean "should".
+    // Is it a correct way to do that? Means: Is it legal and it should be done like that?
+    
+    //TODO: Add correct usage of assert keyword Page 392-394
+    
+    
   }
 }
