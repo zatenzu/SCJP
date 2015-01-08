c4050b6b3d625f270c50be5a8cb204bc3848d046
zatenzu <zatenzu@hotmail.com>
2015-01-08 13:43:29 +0100

    + try block alone, codes between blocks

--- src/be/zatenzu/scjp/keywords/_try/_Try.java
+++ src/be/zatenzu/scjp/keywords/_try/_Try.java
@@ -1,7 +1,27 @@
 // $Id$
 package be.zatenzu.scjp.keywords._try;
 
+
 public class _Try
 {
-  //see chapter 5, exception
+  public static void main(String[] args)
+  {
+    //try cannot be used alone
+//    try{ 
+//    }
+    //Forbidden
+    
+    //You cannot put some code between try and catch or finally block!
+//    try{
+//      
+//    }
+//    System.out.println("Forbidden");
+//    catch(Exception ex){
+//      
+//    }
+//    System.out.println("Forbidden");
+//    finally{
+//    }
+    //See finally and catch keywords
+  }
 }
