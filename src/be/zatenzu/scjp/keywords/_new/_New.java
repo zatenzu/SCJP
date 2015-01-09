17d27ba6adec43c539d47eef403557fe75a9b40a
zatenzu <zatenzu@hotmail.com>
2015-01-09 17:37:29 +0100

    + What happends during new ?

--- src/be/zatenzu/scjp/keywords/_new/_New.java
+++ src/be/zatenzu/scjp/keywords/_new/_New.java
@@ -8,4 +8,12 @@ public class _New
    */
   Object o = new Object();
   int[] tab = new int[2];
+  
+  /** What happends during new ?**/
+  //- All instance variables are assigned default values
+  //- The constructor is invoked and the super constructor too etc...
+  //- All super constructors complete
+  //- Instance variables that are initialized as part of their declaration are assigned
+  //their initial value.
+  //- The constructor completes
 }
