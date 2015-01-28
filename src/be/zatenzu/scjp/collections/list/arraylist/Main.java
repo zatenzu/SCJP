90435d23dc427272cb9df8ace656ec276dca37f6
zatenzu <zatenzu@hotmail.com>
2015-01-28 19:00:11 +0100

    + Initial import

---
+++ src/be/zatenzu/scjp/collections/list/arraylist/Main.java
@@ -0,0 +1,13 @@
+// $Id$
+package be.zatenzu.scjp.collections.list.arraylist;
+
+import java.util.ArrayList;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    //implements interface List
+    ArrayList<String> al = new ArrayList<>();
+  }
+}
---
+++ src/be/zatenzu/scjp/collections/list/vector/Main.java
@@ -0,0 +1,10 @@
+// $Id$
+package be.zatenzu.scjp.collections.list.vector;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    //Samething as ArrayList but synchronized
+  }
+}
