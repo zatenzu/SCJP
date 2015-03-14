12e07bf89c980cf7aba0de32b0a73383ecfbd071
zatenzu <zatenzu@hotmail.com>
2015-03-14 16:42:53 +0100

    + development

---
+++ src/be/zatenzu/scjp/development/Java.java
@@ -0,0 +1,13 @@
+// $Id$
+package be.zatenzu.scjp.development;
+
+public class Java
+{
+  /** java [options] class [args] **/
+  //java MyClass.class or java MyClass is the same
+  //java -DmyVar=YOU MyClass "Hello World -> run MyClass with myVar as system property and
+  //"Hello World" as argument to the main method
+  
+  //if you class is in a package --> use classpath !!
+//  java -cp classes/ -DmyVar=YOU be.zatenzu.test.A "Hello World"
+}
---
+++ src/be/zatenzu/scjp/development/Javac.java
@@ -0,0 +1,19 @@
+// $Id$
+package be.zatenzu.scjp.development;
+
+public class Javac
+{
+  /** javac [options] [source files] **/
+  //-d
+  //say to the javac where to put compiled files
+  //without this option, it puts compiles files in the same directory
+  //as source files
+  //if the specified source file is in a package, the javac command create
+  //the same structure in your destination directory.
+  
+  //-ea and -da
+  //@see assert keyword
+  
+  //-help
+  //send SOS to the police...
+}
