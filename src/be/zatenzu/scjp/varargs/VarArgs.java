17f52adec40319e44fef1924c85b6fb74ea17169
zatenzu <zatenzu@hotmail.com>
2015-03-17 16:26:26 +0100

    - array != varargs

--- src/be/zatenzu/scjp/varargs/VarArgs.java
+++ src/be/zatenzu/scjp/varargs/VarArgs.java
@@ -9,6 +9,7 @@ public class VarArgs
   public void myMethod1(int a ,String ... args){};//with space between String and ellipsi(...)
   
   /* Illegal
+  public void myMethod(String[] args){};//Array is not varargs (but main(String[] or String...) It's the same) ...
   public void myMethod(String args...){};//Ellipsis must be after parameters type
   public void myMethod(String... args, int a){};//var-args must bt the last parameter
   public void myMethod(String... args, int... as){};//Only one var-args parameter
