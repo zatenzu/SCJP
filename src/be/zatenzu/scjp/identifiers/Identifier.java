cedbd8e5307f17219b22578774b939e7c1d97deb
zatenzu <zatenzu@hotmail.com>
2015-03-17 16:41:38 +0100

    + JavaBeans

--- src/be/zatenzu/scjp/identifiers/Identifier.java
+++ src/be/zatenzu/scjp/identifiers/Identifier.java
@@ -24,4 +24,32 @@ public class Identifier
   public String a+;//cannot use operators
   public String strictfp;// /!\ keywords! @see keyword package
   */
+  
+  /** JavaBeans standards **/
+  
+  //GET
+  //getVarName()
+  // - no argument
+  // - return the type of the var
+  // - camel case
+  // - must be public
+  
+  //IS
+  //must be isVarName()
+  // - only if the var is a boolean PRIMITIVE
+  // - return a boolean PRIMITIVE
+  // - camel case
+  // - must be public
+  
+  //SET
+  // - must be setVarName(vartype var)
+  // - Only 1 argument with the same type as var
+  // - camel case
+  // - must be public
+  
+  //Listener
+  // add/removeActionListener
+  // - must start with add or remove
+  // - must end with Listener
+  // - must take 1 argument type of Listener
 }
