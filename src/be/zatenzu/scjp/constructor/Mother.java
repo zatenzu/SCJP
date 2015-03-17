af75507ec4aa193148ed23fb7c19a0dba095dafd
zatenzu <zatenzu@hotmail.com>
2015-03-17 16:42:39 +0100

    + default constructor

--- src/be/zatenzu/scjp/constructor/Mother.java
+++ src/be/zatenzu/scjp/constructor/Mother.java
@@ -3,6 +3,10 @@ package be.zatenzu.scjp.constructor;
 
 public class Mother
 {
+  protected Mother(){
+    System.out.println("Mother constructor");
+  }
+  
   protected Mother(int i){
     System.out.println("Mother constructor");
   }
