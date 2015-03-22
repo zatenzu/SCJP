dd6af38a1bdf85eaa7f992f80b7838b6b4b05bd0
zatenzu <zatenzu@hotmail.com>
2015-03-22 15:54:57 +0100

    + return type

--- src/be/zatenzu/scjp/keywords/_retrurn/_Return.java
+++ src/be/zatenzu/scjp/keywords/_retrurn/_Return.java
@@ -1,7 +1,39 @@
 // $Id$
 package be.zatenzu.scjp.keywords._retrurn;
 
-public class _Return<T>
+public class _Return
 {
-  /** nothing special **/
+  //return type follow wrapper et primitive type rules
+  public void myVoidMethod(){
+    return;//exit the method
+  }
+  
+  public Mother myReturnMotherMethod(){
+    return new SubClass();//IS-A test ok
+  }
+  
+  public SubClass myReturnSubClassMethod(){
+//    return new Mother();//cannot without casting
+    return new SubClass();
+  }
+  
+  public int myReturnIntMethod(){
+    return new Integer(1);//Can return the wrapper
+  }
+  
+  public Integer myReturnWrapperClass(){
+    int i = 2;
+    return i;//Can return the primitive type
+  }
+  
+  public int myReturnWithWideningMethod(){
+    byte b = 1;
+    return b;//widening ok
+  }
+  
+  //cannont return var args
+  /*public String... myVarArgsMethod(){
+    return "";
+  }*/
+  
 }
