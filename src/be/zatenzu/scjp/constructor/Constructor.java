30f0562d66214bb8ed1d65fac36116133cc037e4
zatenzu <zatenzu@hotmail.com>
2015-03-26 18:35:49 +0100

    - anymous code block run before each constr. call

--- src/be/zatenzu/scjp/constructor/Constructor.java
+++ src/be/zatenzu/scjp/constructor/Constructor.java
@@ -3,16 +3,22 @@ package be.zatenzu.scjp.constructor;
 
 public class Constructor
 {
+//  final blank var must initialize in each constructors
+  final int j;
+  
   static{
-    System.out.println("This block runs before constructor call");
+    System.out.println("This block runs, one time before constructor call");
   }
   
   {
-    System.out.println("This block runs after constructor call");
+    System.out.println("This block runs after each constructor call");
   }
   //All access modifiers are allowed
   //Constructor must have the same name as the Class
-  public Constructor(){}
+  public Constructor(){
+    System.out.println("This block runs during constructor call");
+    j=0;
+    }
   //if no constructor is defined in the class, this default constructor is implicit!
 
   //the auto-created(implicit default constructor) constructor will:
@@ -21,16 +27,16 @@ public class Constructor
   // -have as first line super();ex Constructor(){super();}
   
   //can be protected
-  protected Constructor(int i){}
+  protected Constructor(int i){j=0;}
   
   //can be default package
-  Constructor(float i){}
+  Constructor(float i){j=0;}
   
   //can be private
-  private Constructor(int i, int a){}
+  private Constructor(int i, int a){j=0;}
   
   //can have var-arg parameter
-  public Constructor(String... args){}
+  public Constructor(String... args){j=0;}
   
 //Constructor can't ever have a return type.
 //Here, there is a return type but it's not a constructor, it's a badly named method.
@@ -44,4 +50,9 @@ public class Constructor
   
   //A constructor can't be final!
 //public final Constructor(){};
+  
+  public static void main(String[] args)
+  {
+    new Constructor();
+  }
 }
